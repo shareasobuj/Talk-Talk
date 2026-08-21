ef382d3508cbe399c055f65bd177975a9ca03443
SHAREA SOBUJ <shareasobuj9829@gmail.com>
2026-08-22 00:31:48 +0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'talktalk-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html', './style.css', './script.js', './manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
